0f8950a2cfd814e210c1cf908457aaad0005633f
indhu1105 <indhumathi.mani@trustrace.com>
2020-05-20 17:53:20 +0530

    Add js file

---
+++ rec.js
@@ -0,0 +1,9 @@
+let length = +prompt("enter the length");
+        let width = +prompt("enter the width");
+        let result;
+
+        function area() {
+          result = length * width;
+          console.log(result);
+          return result;
+        }
